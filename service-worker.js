825da882a17188053388b2b7cfa0954cd2a5c153
Univermielnium <8232300128@univermilenium.edu.mx>
2026-02-19 09:25:47 -0600

    Add service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'reservas-av-v1';
+const urlsToCache = [
+  './reservacion.html',
+  './manifest.json',
+  'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.3/jspdf.plugin.autotable.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
